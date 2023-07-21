505c22b4c51da3da66dde216bfd8d3b742356d72
patrakhin <patrakhin@mail.ru>
2023-07-21 20:21:52 +0300

    simple controller add

---
+++ src/main/java/ru/patrakhin/sringbootapp/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package ru.patrakhin.sringbootapp.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
